daaad226d07c36d8c41e86a99fa6f5ccb512d7bc
Alona Kharchenko <aliona.kharchenko@gmail.com>
2019-06-11 19:16:34 +0200

    bugfix

--- src/muscle/IMuscle.cpp
+++ src/muscle/IMuscle.cpp
@@ -92,8 +92,11 @@ namespace cardsflow_gazebo {
         motor.setIntegrator(
                 Actuator::RungeKutta4);
         motor.setTimeStep(0.0001);
-
+        
+        #ifdef ENABLE_LOGGING
         save_srv = nh->advertiseService("/roboy/simulation/SaveData/"+name, &IMuscle::saveDataService, this);
+        #endif
+        
         ROS_INFO_STREAM("Initialized muscle with id: " << muscleID);
     }
 
