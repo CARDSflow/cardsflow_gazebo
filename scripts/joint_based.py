0fc268ab6ac11a1efaee14c4ed3ce0096aa5e65b
missxa <aliona.kharchenko@gmail.com>
2019-12-05 09:13:02 +0000

    added HW interface

--- scripts/joint_based.py
+++ scripts/joint_based.py
@@ -1,6 +1,9 @@
 import rospy
 from sensor_msgs.msg import JointState
+from roboy_middleware_msgs.msg import JointStatus
 from roboy_middleware_msgs.msg import MotorCommand
+from roboy_middleware_msgs.srv import ControlMode, ControlModeRequest
+
 from math import tanh, sqrt, sin
 import numpy as np
 
@@ -10,9 +13,46 @@ def softmax(x):
 rospy.init_node("musctest")
 
 p = rospy.Publisher('/roboy/middleware/MotorCommand', MotorCommand, queue_size=1)
-max_cmd = 240
+change_control_mode = rospy.ServiceProxy('/roboy/shoulder_left/middleware/ControlMode', ControlMode)
+req = ControlModeRequest()
+req.motor_id = [0,1,2,3]
+req.control_mode = 2 # DISPLACEMENT
+change_control_mode(req)
+
+max_cmd = [100,20]
+
 msg = MotorCommand()
-msg.motors = [0,1,2,3]
+msg.id = 3
+
+def encoderticks2degrees(ticks):
+    return ticks*(360.0/4096.0)
+
+center = [2673.5, 2539.0]
+motors_map = {0: [0,1], 1: [2,3]}
+
+def cb2(data):
+    angles = [] # in degrees [lower, upper]
+    for i in [0,1]: #range(len(center)):
+        encoderticks = data.absAngles[i] - center[i]
+        angle = encoderticks2degrees(encoderticks)
+        angles.append(angle)
+        motors = motors_map[i]
+        msg.motors.extend(motors)
+        if angle<0:
+            msg.set_points.append(abs(sin(angle)*max_cmd[i]))
+            msg.set_points.append(0)
+        else:
+            msg.set_points.extend([0, abs(sin(angle)*max_cmd[i])])
+
+    p.publish(msg)
+    print angles
+    print msg
+    msg.set_points = []
+    msg.motors = []
+
+
+
+
 
 def cb(data):
     msg.set_points = [0,0,0,0]
@@ -28,5 +68,6 @@ def cb(data):
         msg.set_points[3] = sin(abs(data.position[0]))*5
     p.publish(msg)    
 
+s_hw = rospy.Subscriber('/roboy/middleware/JointStatus', JointStatus, cb2) 
 s = rospy.Subscriber('/joint_states', JointState, cb)
 rospy.spin()
