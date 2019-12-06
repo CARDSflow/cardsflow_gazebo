0948502781f02a52c1a7298075aa00bad29fd059
Alona Kharchenko <aliona.kharchenko@gmail.com>
2019-12-06 11:58:35 +0100

    Update simple_ctl.py

--- scripts/simple_ctl.py
+++ scripts/simple_ctl.py
@@ -12,33 +12,37 @@ rospy.loginfo("Changing control mode to DISPLACEMENT")
 change_control_mode = rospy.ServiceProxy('/roboy/shoulder_left/middleware/ControlMode', ControlMode)
 req = ControlModeRequest()
 req.motor_id = [0,1,2,3]
-req.control_mode = 2 # DISPLACEMENT
+req.control_mode = 1 # VELOCITY
 change_control_mode(req)
 rospy.loginfo("Changed to DISPLACEMENT control mode")
-
-max_cmd = [40,10]
 
 msg = MotorCommand()
 msg.id = 3
 
-def encoderticks2degrees(ticks):
-    return ticks*(360.0/4096.0)
+msg.motors = [0,1]
+msg.set_points = [-100000, 100000]
+p.publish(msg)
+msg.motors = []
+msg.set_points = []
 
 center = [2673.5, 2539.0]
 motors_map = {0: [0,1], 1: [2,3]}
 
+def encoderticks2degrees(ticks):
+    return ticks*(360.0/4096.0)
+
 def cb(data):
     angles = [] # in degrees [lower, upper]
-    for i in [0,1]: #range(len(center)):
+    for i in [0]: #range(len(center)):
         encoderticks = data.absAngles[i] - center[i]
         angle = encoderticks2degrees(encoderticks)
         angles.append(angle)
         motors = motors_map[i]
         msg.motors.extend(motors)
-        if angle<0:
-            msg.set_points.extend([abs(sin(angle)*max_cmd[i]),0])
-        else:
-            msg.set_points.extend([0, abs(sin(angle)*max_cmd[i])])
+        if angle <= -50:
+            msg.set_points.extend([-100000, 100000])
+        elif angle >= 50:
+            msg.set_points.extend([-100000, 100000])
 
     p.publish(msg)
     #rospy.loginfo("Current joint angle values: " + str(angles))
